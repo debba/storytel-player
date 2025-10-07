8ab2d1597927f02d4d5545f1a5bbc2c2c442d2ae
Andrea Debernardi <andrea@debbaweb.it>
2025-10-07 13:29:15 +0200

    fix: dotenv quiet

--- server/fastify-common.ts
+++ server/fastify-common.ts
@@ -27,7 +27,9 @@ declare module 'fastify' {
 }
 
 
-dotenv.config();
+dotenv.config({
+    quiet: true
+});
 
 const JWT_SECRET = process.env.JWT_SECRET || 'your-super-secret-jwt-key-change-this-in-production';
 
